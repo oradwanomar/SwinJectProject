e9a918d01d649a9d8f01dd3ff9a895be4a22853d
omarradwan <oradwan037@gmail.com>
2022-08-15 15:00:47 +0200

    Begin

---
+++ Pods/Target Support Files/Pods-SwinJectProject-SwinJectProjectUITests/Pods-SwinJectProject-SwinJectProjectUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwinJectProject_SwinJectProjectUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SwinJectProject_SwinJectProjectUITests
+@end
---
+++ SwinJectProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwinJectProject
+//
+//  Created by Omar Ahmed on 15/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
